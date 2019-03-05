fcc5734e74717bf0e2c77b431e5518e1b6d7a5f1
Kyle Jiang <keeler@gmail.com>
2019-03-04 23:45:32 -0500

    heroku try2

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://localhost/acme_faker');
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 const faker = require('faker')
 
 //product fake name generator
